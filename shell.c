dc0a8fe387c74213cf78793f5cf953e6d2f769a1
TutTrue <hamdy.xd55@gmail.com>
2023-05-11 12:24:34 -0700

    main structure of the project

---
+++ shell.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * main:...
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * @env: enviroment
+ * Return: 0
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *line = NULL;
+	size_t buf_size = 0;
+	char **array_command = NULL;
+
+	while(1)
+	{
+		write(STDOUT_FILENO, ">_<$ ", 5);
+		getline(&line, &buf_size, stdin);
+		array_command = get_array_command(line);
+		fork_excute(array_command);
+
+	}
+}
